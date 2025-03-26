962eea7be52cd8cb959ed7113005b7cc320c7818
zdtz <dduythien.it@gmail.com>
2025-03-26 15:52:39 +0700

    update for demo

--- src/screens/home/mock/MockData.ts
+++ src/screens/home/mock/MockData.ts
@@ -1,30 +1,28 @@
 export default [
   {
-    name: "React Native Dynamic Search Bar",
-    description:
-      "Dynamic fully customizable search bar for both iOS and Android",
+    name: "Item 1",
+    description: "SL rn start kit",
     language: "Typescript",
     star: 342,
     fork: 152,
   },
   {
-    name: "React Native Bouncy Checkbox",
-    description: "Fully customizable animated bouncy checkbox",
+    name: "Item 2",
+    description: "SL rn start kit",
     language: "Typescript",
     star: 272,
     fork: 22,
   },
   {
-    name: "React Native Apple Card Views",
-    description:
-      "Customizable and perfect clone of Apple, App Store Card Views",
+    name: "Item 3",
+    description: "SL rn start kit",
     language: "Typescript",
     star: 266,
     fork: 22,
   },
   {
-    name: "React Native Header View",
-    description: "Fully customizable Header View with multiple design options ",
+    name: "Item 4",
+    description: "SL rn start kit",
     language: "Typescript",
     star: 412,
     fork: 28,
